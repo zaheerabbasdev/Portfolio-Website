94a726e6367dd0f833c2635da7f36c0a80d5cb94
Zaheerabbas <zabbas092002@gmail.com>
2024-09-28 21:42:23 +0500

    Add the text in Courses section

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -25,6 +25,27 @@ const coursesData = [
       "Completed the Freelancing course on DigiSkills, focusing on building a successful freelance career. Learned key strategies for managing freelance projects, communicating with clients in a competitive online marketplace. The course provided practical insights into various freelancing platforms, enabling me to offer services like Python scripting and data analysis.",
     link: "https://lms.digiskills.pk/MyResults/MyResults.aspx",
   },
+  {
+    title: "Digitall Marketing",
+    platform: "DigiSkills",
+    description:
+      "Completed the Digital Marketing course on DigiSkills, where I gained essential knowledge in effective marketing strategies for the digital landscape. Developed skills in creating compelling content and analyzing marketing data to measure campaign success. This experience equips me with the skills to promote products and services effectively in today's digital marketplace.",
+    link: "https://lms.digiskills.pk/MyResults/MyResults.aspx",
+  },
+   {
+    title: "WordPress",
+    platform: "DigiSkills",
+    description:
+      "Completed the WordPress course on DigiSkills, where I acquired comprehensive knowledge of WordPress development and management. Learned to create and customize professional websites. Developed skills in content creation, ensuring websites are engaging and optimized for search engines. The course also covered best practices for maintaining and securing WordPress sites.",
+    link: "https://lms.digiskills.pk/MyResults/MyResults.aspx",
+  },
+   {
+    title: "Graphic Designing",
+    platform: "DigiSkills",
+    description:
+      "Completed the Graphic Designing course on DigiSkills, where I gained a solid foundation in design principles and techniques. Learned to use industry-standard software such as Adobe Photoshop and Illustrator. The course emphasized creativity and innovation. This training has equipped me to create compelling visuals that communicate messages effectively.",
+    link: "https://lms.digiskills.pk/MyResults/MyResults.aspx",
+  },
 ];
 
 const Courses = () => {
